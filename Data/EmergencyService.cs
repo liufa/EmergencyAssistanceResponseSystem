ddff35684bfbe5b394fbe1df43e741116e6efcb7
liufa <mataswork@gmail.com>
2016-01-08 16:18:22 +0000

    Added mechanism to send and update coordinates

---
+++ Data/EmergencyService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Spatial;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class EmergencyService
+    {
+        public void CreateOrUpdateLocation(int crewId, string location, string route)
+        {
+            using (var db = new EarsEntities())
+            {
+                var callout = db.Callout.FirstOrDefault(o => o.Route == route);
+                if (callout != null)
+                {
+                    callout.Location = DbGeography.FromText(location);
+                    db.Entry(callout).Property(o => o.Location).IsModified = true;
+                }
+                else {
+                    db.Callout.Add(new Callout { Crew = crewId, Location = DbGeography.FromText(location), Route = route });
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
--- Services/Controllers/EmergencyServiceController.cs
+++ Services/Controllers/EmergencyServiceController.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Data.Entity.Spatial;
 
 namespace Services.Controllers
 {
     public class EmergencyServiceController : Controller
     {
-        public ActionResult Callout(string crewId, string route, string coordinates)
+        public ActionResult Callout(int crewId,  string coordinates, string route)
         {
+            var coordinateArray = coordinates.Split(new[] { ',' });
+            var location = string.Format("POINT({0} {1})", coordinateArray[0], coordinateArray[1]);
+            new EmergencyService().CreateOrUpdateLocation(crewId, location, route);
+
             return null;
         }
     }
