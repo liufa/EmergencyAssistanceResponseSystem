b023d892e1871224df45e82d41a1f50ec08df217
liufa <mataswork@gmail.com>
2016-01-08 14:43:42 +0000

    Added services Files

---
+++ Services/Controllers/EmergencyServiceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Services.Controllers
+{
+    public class EmergencyServiceController : Controller
+    {
+        public ActionResult Callout(string crewId, string route, string coordinates)
+        {
+            return null;
+        }
+    }
+}
